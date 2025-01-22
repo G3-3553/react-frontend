b1e50169055571a4f63a4165569b3b68a588e8e9
Gayathri <g3saga5335@gmail.com>
2025-01-22 11:34:30 +0530

    Fix import path for IntroPage.css

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './intropage.css';
+import './IntroPage.css';
 
 const IntroPage = () => {
   const navigate = useNavigate();
